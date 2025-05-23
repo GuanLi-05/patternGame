0445503494e224ddb1946fef20955544eb001272
Guan Li <guanLi@student.unsw.edu.au>
2025-05-23 19:40:33 +1000

    basic arithmetic sequence

--- pattern-game/src/GameHandler.jsx
+++ pattern-game/src/GameHandler.jsx
@@ -3,28 +3,34 @@ import React from 'react'
 export default function GameHandler() {
   const STARTING_RANGE = 26;
   const PATTERN_LENGTH = 5;
-  const [patterFunctions, setPatternFunctions] = React.useState([]);
+  const storage = React.useRef({});
+  const [patternFunctions, setPatternFunctions] = React.useState([]);
   const [patternTerms, setPatternTerms] = React.useState([]);
   const [game, setGame] = React.useState(0);
 
-  // initialise all pattern functions into patterFunctions
+  // initialise all pattern functions into patternFunctions
   React.useEffect(() => {
-    setPatternFunctions([pattern1, pattern2]);
+    setPatternFunctions([
+      arithmeticPattern, 
+      pattern2
+    ]);
   }, []);
 
-  // generate pattern of length PATTERN_LENGTH, from 0 to STARTING_RANGE -1 
+  // New Game: generate pattern of length PATTERN_LENGTH, from 0 to STARTING_RANGE -1 
+  // clear other data
   React.useEffect(() => {
-    generatePattern(Math.trunc(Math.random() * STARTING_RANGE), PATTERN_LENGTH)
+    generatePattern(Math.trunc(Math.random() * STARTING_RANGE), PATTERN_LENGTH);
+    storage.current= {};
   }, [game]);
 
-  // pick random pattern from patterFunctions and generate sequence into patternTerms
+  // pick random pattern from patternFunctions and generate sequence into patternTerms
   const generatePattern = (input, numberOfTerms) => {
-    if (patterFunctions.length === 0) return;
+    if (patternFunctions.length === 0) return;
 
-    const randFunc = patterFunctions[Math.trunc(Math.random() * patterFunctions.length)];
+    const randFunc = patternFunctions[Math.trunc(Math.random() * patternFunctions.length)];
     const temp = [input];
     for (let i = 0; i < numberOfTerms - 1; i++) {
-      temp.push(randFunc(temp[i]));
+      temp.push(randFunc(temp[i], storage));
     }
     setPatternTerms(temp);
   }
@@ -43,8 +49,25 @@ export default function GameHandler() {
   )
 }
 
-function pattern1(input) {
-  return input + 1;
+/////////////////////////////////////////
+/// Patterns
+/////////////////////////////////////////
+
+// Pattern functions must only have parameters: (input, storage)
+
+/* 
+ * Generate number from 1-15
+ * Sequence adds by that number
+ * 
+ * Example (+7): 0, 7, 14, 21, 28
+ */
+function arithmeticPattern(input, storage) {
+  if (!storage.current.arithmetic) {
+    const a = Math.trunc(Math.random() * 16);
+    storage.current = {...storage.current, arithmetic: a};
+    return input + a;
+  }
+  return input + storage.current.arithmetic;
 }
 
 function pattern2(input) {
